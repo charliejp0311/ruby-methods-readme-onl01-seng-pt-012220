d526baa5d6bd8bdfff56cc519e65f2d9f3fec4ba
Charles Pelton <charliejp0311@gmail.com>
2020-01-21 15:38:25 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,24 @@
 # Use this space to code along with the readme
+def say_hello_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
 
 puts phrase
 puts phrase
@@ -23,4 +31,5 @@ puts phrase
 puts phrase
 puts phrase
 
-def say_hello
+
+  
